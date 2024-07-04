58bed483ef7c8ad3014f92032ebf0ad4de820321
Mavi <160594705+maviasalemuhmmad@users.noreply.github.com>
2024-07-04 09:10:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+"use strict";
+// let a = 0
+// if (a < 1 ){
+Object.defineProperty(exports, "__esModule", { value: true });
+// }
+// a++
+// for (let count = 1; count <= 10; count++){
+//     console.log ("Hello world ")
+//     console.log ("1 ka sum")
+// }
+// const printTable = (num : number) =>{ 
+//     for (let i = 1; i <= 10; i++ ){
+//         console.log(`${num}*${i}= ${num * i}`);
+//     }
+// };
+// printTable(10);
+// let num = 11;
+//////////////////////////////////////////////////////////////////
+// const SumofN = (num : number) => {
+//     let sum = 0 ; 
+//     for (let j = 1; j <= num; j++ ){
+//         sum = sum + j;
+//     }
+//     console.log(sum)
+// };
+// SumofN(5)
+//////////////////////////////////////////////////////////////////
+// let i = 0
+// // 0 <=10  
+// while(i < 10 ){
+//    console.log(i, "i");
+//     i++;
+// }
+////////////////////////////////////////////////////////////////////
+// let a = 1 
+// let tableNum = 2
+// while (a <= 10 ){
+// console.log(`${tableNum} * ${a} = ${tableNum *a}`);
+// a++;
+// }
+/////////////////////////////////////////////////////////////////////
+// let obj = {
+//     name : "Mavi",
+//     batch : "Thursday class",
+// };
+// console.log(obj.name );
+// console.log(obj ["name"]);
+// let student = {
+//     name : "Mavi",
+//     batch : "Thursday class",
+// isAvailable : false,
+// };
+// for (let x in student){
+//     console.log(x , "x");
+// }
+///////////////////////////////////////////////////////////////////////
+// let student = {
+//     name : "Mavi",
+//     batch : "Thursday class",
+// isAvailable : false,
+// };
+// student["name"]
+// for (let x in student){
+//     console.log(x, student [x]);
+// }
+var student = {
+    Name: "Mavia",
+};
+"Salman",
+    Class;
+"Matric",
+    Number;
+"03242214905",
+    Address;
+"shahlatef town",
+    isAvalibale;
+false,
+;
+;
+student["name"];
+for (var x in student) {
+    console.log(x, student[x]);
+}
+;
---
+++ main.ts
@@ -0,0 +1,84 @@
+// let a = 0
+// if (a < 1 ){
+
+
+
+// }
+// a++
+// for (let count = 1; count <= 10; count++){
+//     console.log ("Hello world ")
+//     console.log ("1 ka sum")
+// }
+
+// const printTable = (num : number) =>{ 
+//     for (let i = 1; i <= 10; i++ ){
+//         console.log(`${num}*${i}= ${num * i}`);
+//     }
+// };
+// printTable(10);
+// let num = 11;
+//////////////////////////////////////////////////////////////////
+
+// const SumofN = (num : number) => {
+//     let sum = 0 ; 
+//     for (let j = 1; j <= num; j++ ){
+//         sum = sum + j;
+//     }
+//     console.log(sum)
+    
+// };
+// SumofN(5)
+//////////////////////////////////////////////////////////////////
+
+// let i = 0
+// // 0 <=10  
+// while(i < 10 ){
+//    console.log(i, "i");
+//     i++;
+// }
+
+////////////////////////////////////////////////////////////////////
+
+// let a = 1 
+
+// let tableNum = 2
+// while (a <= 10 ){
+// console.log(`${tableNum} * ${a} = ${tableNum *a}`);
+// a++;
+// }
+
+/////////////////////////////////////////////////////////////////////
+
+// let obj = {
+//     name : "Mavi",
+//     batch : "Thursday class",
+
+// };
+// console.log(obj.name );
+// console.log(obj ["name"]);
+
+// let student = {
+//     name : "Mavi",
+//     batch : "Thursday class",
+// isAvailable : false,
+// };
+// for (let x in student){
+//     console.log(x , "x");
+    
+// }
+///////////////////////////////////////////////////////////////////////
+
+// let student = {
+//     name : "Mavi",
+//     batch : "Thursday class",
+// isAvailable : false,
+
+// };
+// student["name"]
+// for (let x in student){
+//     console.log(x, student [x]);
+    
+// }
+
+
+
